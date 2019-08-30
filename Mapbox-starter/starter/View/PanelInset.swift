65d17ff925c294d70465b190ac298e490015aa35
Wilson Desimini <wdesimini8@gmail.com>
2019-08-29 19:22:15 -0700

    Fixed half position panel offset bug

--- Mapbox-starter/starter/View/PanelInset.swift
+++ Mapbox-starter/starter/View/PanelInset.swift
@@ -12,8 +12,10 @@ struct PanelInset {
     static let full = Default.padding
     
     static var half: CGFloat {
-        let screenHeight = UIScreen.main.bounds.height
-        return screenHeight / 2
+        // take lesser of 2 screen values, because is based on screen bounds
+        // if you take the screen height on landscape rotation, will be taller than screen
+        let lesser = min(UIScreen.main.bounds.height, UIScreen.main.bounds.width)
+        return lesser / 2
     }
     
     static let tip = (Default.grabberInset * 2) + Default.searchBarHeight
