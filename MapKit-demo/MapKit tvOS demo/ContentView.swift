dad11ea1c9c18e02307b54a46fcbc589d1e46639
Rob | Labs <roblabs@users.noreply.github.com>
2020-01-04 09:08:12 -0800

    Xcode: New tvOS Target

---
+++ MapKit-demo/MapKit tvOS demo/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  MapKit tvOS demo
+//
+//  Created by Rob Labs on 1/4/20.
+//  Copyright © 2020 Rob Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
