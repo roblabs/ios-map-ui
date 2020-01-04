c873757e56440964133cf17cadee6e458f5fbfb2
Rob | Labs <roblabs@users.noreply.github.com>
2020-01-04 09:12:33 -0800

    Xcode:  Add new SwiftUI View to both iOS & tvOS targets

---
+++ MapKit-demo/Common/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  MapKit-demo
+//
+//  Created by Rob Labs on 1/4/20.
+//  Copyright © 2020 Rob Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
