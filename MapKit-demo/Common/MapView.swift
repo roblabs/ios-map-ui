fcfcc296d548a61f03388119bb840b5d0d587dc8
Rob | Labs <roblabs@users.noreply.github.com>
2020-01-04 10:39:02 -0800

    Return a MapKit mapview instead of Text()

--- MapKit-demo/Common/MapView.swift
+++ MapKit-demo/Common/MapView.swift
@@ -7,15 +7,29 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    
+    var mapType: MKMapType
+    var coordinate = CLLocationCoordinate2D(latitude: 33.0, longitude: -117)
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.mapType = mapType
+        return mapView
+    }
+    
+    func updateUIView(_ mapView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+            MapView(mapType: .standard)
     }
 }
--- MapKit-demo/MapKit-demo/ContentView.swift
+++ MapKit-demo/MapKit-demo/ContentView.swift
@@ -60,6 +60,7 @@ struct DetailView: View {
 
     var body: some View {
         Group {
+            MapView(mapType: .standard)
             if selectedDate != nil {
                 Text("\(selectedDate!, formatter: dateFormatter)")
             } else {
