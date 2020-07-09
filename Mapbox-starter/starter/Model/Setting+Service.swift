4bb683dee723f5abadd5accd12d8d93ddb359cce
Wilson Desimini <wdesimini8@gmail.com>
2020-07-09 08:23:42 -0700

    add setting datasource

---
+++ Mapbox-starter/starter/Model/Setting+Service.swift
@@ -0,0 +1,64 @@
+//
+//  Setting+Service.swift
+//  Mapbox-starter
+//
+//  Created by Wilson Desimini on 7/9/20.
+//  Copyright © 2020 ePi Rational, Inc. All rights reserved.
+//
+
+import Foundation
+
+struct SettingDatasource {
+    static let settings: [Setting] = {
+        [
+            Setting(
+                id: "setting_1",
+                title: "Setting 1",
+                subtitle: "Subtitle 1",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_2",
+                title: "Setting 2",
+                subtitle: "Subtitle 2",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_3",
+                title: "Setting 3",
+                subtitle: "Subtitle 3",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_4",
+                title: "Setting 4",
+                subtitle: "Subtitle 4",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_5",
+                title: "Setting 5",
+                subtitle: "Subtitle 5",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_6",
+                title: "Setting 6",
+                subtitle: "Subtitle 6",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_7",
+                title: "Setting 7",
+                subtitle: "Subtitle 7",
+                imageTitle: "GPS_icon"
+            ),
+            Setting(
+                id: "setting_8",
+                title: "Setting 8",
+                subtitle: "Subtitle 8",
+                imageTitle: "GPS_icon"
+            ),
+        ]
+    }()
+}
